35f3c96400ecd29815d3e149a9d75d4b07b43bed
stacksapien <contact.vishalvermachd@gmail.com>
2020-03-13 06:38:15 +0530

    Added Master/Slave clustering

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+var cluster = require('cluster');
+
+if(cluster.isMaster) {
+    var numWorkers = require('os').cpus().length;
+
+    console.log('Master cluster setting up ' + numWorkers + ' workers...');
+
+    for(var i = 0; i < numWorkers; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('online', function(worker) {
+        console.log('Worker ' + worker.process.pid + ' is online');
+    });
+
+    cluster.on('exit', function(worker, code, signal) {
+        console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
+        console.log('Starting a new worker');
+        cluster.fork();
+    });
+} else {
+
 const webCrawler = require('./queue/queue')
 
 if(process.argv.length === 4){
@@ -7,3 +29,5 @@ else{
     console.log("Invalid CLI Arguments");
     
 }
+}
+
